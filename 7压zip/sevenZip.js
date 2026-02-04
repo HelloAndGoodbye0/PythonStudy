91d711ac0f883c588841cdc0a89a1816757490fe
lee <497232807@qq.com>
2026-02-04 16:24:24 +0800

    7z修改

--- 7压zip/sevenZip.js
+++ 7压zip/sevenZip.js
@@ -6,11 +6,15 @@ const sevenZipPath = path.join(__dirname, '7z.exe');
 
 /**
  * 压缩文件
- * @param {*} source 
- * @param {*} destination 
+ * @param {*} source 源文件/目录
+ * @param {*} zipPath  zip文件
  */
-function compressWith7z( source, destination) {
-    let cmd =`${sevenZipPath}  a ${destination} ${source}`
+function compressWith7z( source, zipPath) {
+    if(!fs.existsSync(source)) {
+        console.log(`目录不存在 ${source}`)
+        return
+    }
+    let cmd =`${sevenZipPath}  a ${zipPath} ${source}`
     ChildProcess.execSync(cmd)
 }
 
@@ -18,14 +22,18 @@ function compressWith7z( source, destination) {
 
 /**
  * 解压文件
- * @param {*} source 
- * @param {*} destination 
+ * @param {*} zipPath  zip文件
+ * @param {*} destination  解压目录
  */
-function extractWith7z(source, destination) {
+function extractWith7z(zipPath, destination) {
     if(fs.existsSync(destination)) {
         fs.rmSync(destination, { recursive: true })
     }
-    let cmd =`${sevenZipPath}  x ${source} -o${destination}`
+    if(fs.existsSync(zipPath) == false) {
+        console.log(`文件不存在 ${zipPath}`)
+        return
+    }
+    let cmd =`${sevenZipPath}  x ${zipPath} -o${destination}`
     let res = ChildProcess.execSync(cmd)
     console.log(res.toString())
 }
