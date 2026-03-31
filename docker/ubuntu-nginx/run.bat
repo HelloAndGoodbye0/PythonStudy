7c41eebc3eab0d6baeaa9391fadee998c1b34288
lee <497232807@qq.com>
2026-03-31 17:54:32 +0800

    ``` feat(docker): 优化ubuntu-nginx启动脚本逻辑
    修改run.bat脚本，将原有的删除并重新导出配置文件的方式改为仅在
    配置文件不存在时进行导出操作。这样可以避免每次运行时都删除现
    有配置，保护用户自定义的配置不被覆盖。
    ```

--- docker/ubuntu-nginx/run.bat
+++ docker/ubuntu-nginx/run.bat
@@ -1,21 +1,18 @@
 
 @echo off
-@REM 判断本地存在conf.d目录？删除
-if exist conf.d (
-    ECHO "delete conf.d dir"
-    rmdir /s /q conf.d
-) 
 
-@REM 判断本地存在html目录？删除
-if exist html (
-    ECHO "delete html dir"
-    rmdir /s /q html
-)
-@REM 导出配置文件到当前目录
-docker create --name temp-container ubuntu-nginx
-docker cp temp-container:/etc/nginx/conf.d ./conf.d
-docker cp temp-container:/var/www/html/ ./html
-docker rm temp-container
+
+@REM 判断本地存在conf.d目录？不存在就导出一次
+if not exist conf.d (
+    @REM 导出配置文件到当前目录
+    echo "copying config files..."
+    docker create --name temp-container ubuntu-nginx
+    docker cp temp-container:/etc/nginx/conf.d ./conf.d
+    docker cp temp-container:/var/www/html/ ./html
+    docker rm temp-container
+    ) 
+
+
 @REM 运行容器
 docker run --rm -d -p 80:80 -p 443:443 -v %cd%/conf.d:/etc/nginx/conf.d -v %cd%/html:/var/www/html --name my-ubuntu-nginx ubuntu-nginx
 pause
