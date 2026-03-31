9049a4ad813f04dd60e7a78bfeb59413f582e220
lee <497232807@qq.com>
2026-03-31 17:50:37 +0800

    ``` feat(docker): 添加nginx欢迎页面并改进Windows运行脚本
    添加了标准的nginx欢迎页面index.nginx-debian.html，用于展示nginx服务器
    成功安装的信息。
    
    改进了Windows批处理脚本run.bat，增加了以下功能：
    - 自动清理本地conf.d和html目录
    - 从容器中导出nginx配置文件和HTML内容到本地
    - 支持HTTPS端口443映射
    - 实现配置文件和静态内容的卷挂载
    - 提供更好的容器化部署体验
    ```

--- docker/ubuntu-nginx/run.bat
+++ docker/ubuntu-nginx/run.bat
@@ -1,2 +1,21 @@
-docker run  --rm -d -p 80:80 --name my-ubuntu-nginx ubuntu-nginx 
+
+@echo off
+@REM 判断本地存在conf.d目录？删除
+if exist conf.d (
+    ECHO "delete conf.d dir"
+    rmdir /s /q conf.d
+) 
+
+@REM 判断本地存在html目录？删除
+if exist html (
+    ECHO "delete html dir"
+    rmdir /s /q html
+)
+@REM 导出配置文件到当前目录
+docker create --name temp-container ubuntu-nginx
+docker cp temp-container:/etc/nginx/conf.d ./conf.d
+docker cp temp-container:/var/www/html/ ./html
+docker rm temp-container
+@REM 运行容器
+docker run --rm -d -p 80:80 -p 443:443 -v %cd%/conf.d:/etc/nginx/conf.d -v %cd%/html:/var/www/html --name my-ubuntu-nginx ubuntu-nginx
 pause
